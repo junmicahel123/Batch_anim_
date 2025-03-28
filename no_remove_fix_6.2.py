4d2fe18a7edb91aea3295207051419d4743eb167
Owen <juanmig134187@gmail.com>
2025-03-28 18:12:02 +0800

    pseudocodeist_6.2

---
+++ no_remove_fix_6.2.py
@@ -0,0 +1,7 @@
+#Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
+
+
+# Define a function `custom_removeprefix` that removes a given prefix from a string.
+# chfeck if the string starts with the given prefix.
+# If it does, return the substring excluding the prefix.
+# Otherwise, return the original string.
