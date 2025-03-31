1f3aa774bfd98c8fc42ce374fd80b98d1676a5db
Owen <juanmig134187@gmail.com>
2025-03-31 22:08:32 +0800

    change_

--- no_r_strip_7.1.py
+++ no_r_strip_7.1.py
@@ -6,14 +6,14 @@
 #         MOVE left
 #     RETURN substring up to last non-space character
 
-def custom_rstrip(s):
-    index = len(s) - 1  
-    while index >= 0 and s[index] == ' ':  
+def custom_rstrip(string):
+    index = len(string) - 1  
+    while index >= 0 and string[index] == ' ':  
         index -= 1  
-    return s[:index + 1]  
+    return string[:index + 1]  
 
 
-text = input("Enter a String:  ")
+text = input("Enter a String:")
 trimmed_text = custom_rstrip(text)
 print(f'Original: "{text}"')
 print(f'Trimmed: "{trimmed_text}"')
